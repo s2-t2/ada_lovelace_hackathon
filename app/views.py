d8fca46c9f9a85c39c2e900a5fc3c1de90824b53
sujata5538 <sujata.tamang.5538@student.uu.se>
2017-11-18 14:22:21 +0100

    kb chatbot

--- app/views.py
+++ app/views.py
@@ -37,10 +37,10 @@ def incoming():
         print('Received this message from user %d (%s): %s' % (msg.sender.id, msg.sender.first_name, msg.text))
         chat_id = msg.chat.id
         print('Responding to chat %i using token %s' % (chat_id, bot.token))
-        response_text = kb.ask_uppsala(msg)
+        response_text = process_text(msg)
         resp = bot.send_message(
             chat_id=chat_id,
-            text='helloooo',
+            text= kb.ask_uppsala(response_text),
             parse_mode=None,
             disable_web_page_preview=None,
             reply_to_message_id=None,
