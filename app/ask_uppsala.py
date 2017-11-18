863a2ec543fbd977bfb4ab144a48bb9715aa318f
sujata5538 <sujata.tamang.5538@student.uu.se>
2017-11-18 13:26:11 +0100

    ask_uppsala.py

--- app/ask_uppsala.py
+++ app/ask_uppsala.py
@@ -2,11 +2,27 @@
 # ask_uppsala.py
 # Created by Group #6 on 18 of November 2017.
 
-def ask_uppsala (input):
+count = 0;
 
+
+def ask_uppsala (input):
+    inputList = input.split(',')
+    print(inputList)
+    keywords = ["Uppsala","University","found" ,"founded","when","who","UU","city","Tell","nation","student nation","courses","programme","what"]
+
+    for inpWord in inputList :
+        check = inpWord
+        print(check)
+        for word in keywords:
+            if check == word :
+                print (word)
     info = ""
 
     ## algoritmh comes here
 
 
-    return info
+
+    return ""
+
+ques = "When was University founded?"
+ask_uppsala(ques)
