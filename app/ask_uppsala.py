64899f841e72f7bc1f6304fa06afb29fa5c3c645
sujata5538 <sujata.tamang.5538@student.uu.se>
2017-11-18 13:49:02 +0100

    keywords inputs

--- app/ask_uppsala.py
+++ app/ask_uppsala.py
@@ -2,27 +2,33 @@
 # ask_uppsala.py
 # Created by Group #6 on 18 of November 2017.
 
-count = 0;
-
-
-def ask_uppsala (input):
-    inputList = input.split(',')
-    print(inputList)
-    keywords = ["Uppsala","University","found" ,"founded","when","who","UU","city","Tell","nation","student nation","courses","programme","what"]
-
-    for inpWord in inputList :
-        check = inpWord
-        print(check)
-        for word in keywords:
-            if check == word :
-                print (word)
+def ask_uppsala(input):
+    count = 0
     info = ""
 
+    inputList = input.split(' ')
+    print(inputList)
+    keywords = ["Uppsala",
+                "University",
+                "found", "founded", "when", "who", "UU", "city", "Tell", "nation",
+                "student nation", "courses", "programme", "what"]
+
+    for inpWord in inputList:
+        check = inpWord
+        #print(check)
+        for word in keywords:
+            if check == word:
+                count = count +1
+                print(count)
+
+
+    print (count)
+    if count >= 3 :
+        info = "UU was founded in 1447"
+
     ## algoritmh comes here
+    return info
 
-
-
-    return ""
 
 ques = "When was University founded?"
-ask_uppsala(ques)
+print (ask_uppsala(ques))
